9bf03c0f1313863e013983a60437c90082e4b228
alilopez <ali.lopez37@gmail.com>
2023-06-24 02:06:38 -0600

    fix: add amqplib

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Signale } from "signale";
 
+import { loadRouter } from "./event/LoadRouter";
 import { productRouter } from "./product/infrastructure/ProductRouter";
 
 const app = express();
@@ -9,6 +10,7 @@ const signale = new Signale();
 
 app.use(express.json());
 app.use("/products", productRouter);
+app.use("/load", loadRouter);
 
 app.listen(3000, () => {
   signale.success("Server online in port 3000");
